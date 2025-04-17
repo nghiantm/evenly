efa89d62791e9364477d9220282378cc24d565e3
nghiantm <nguyentrongminhnghia@gmail.com>
2025-04-16 22:30:24 -0400

    Created Expense data model

---
+++ evenly_backend/src/main/java/com/evenly/entity/Expense.java
@@ -0,0 +1,38 @@
+package com.evenly.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", unique = true, length = 36)
+    private String id;
+
+    @Column(nullable = false)
+    private String groupId;
+
+    @Column(nullable = false)
+    private String paidBy;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Date createdDate;
+}

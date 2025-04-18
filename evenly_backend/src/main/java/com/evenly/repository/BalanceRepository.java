03496d2098b8b01bc33bfb151e8ed49d5de68ce1
nghiantm <nguyentrongminhnghia@gmail.com>
2025-04-18 10:56:24 -0400

    BalanceRepository.java - Created jpa repo for Balance table

---
+++ evenly_backend/src/main/java/com/evenly/repository/BalanceRepository.java
@@ -0,0 +1,7 @@
+package com.evenly.repository;
+
+import com.evenly.entity.Balance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BalanceRepository extends JpaRepository<Balance, Long> {
+}

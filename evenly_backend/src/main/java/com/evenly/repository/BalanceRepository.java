673742ffed74cda088b1a27bb6b810351fa360c5
nghiantm <nguyentrongminhnghia@gmail.com>
2025-04-18 11:02:13 -0400

    BalanceRepository.java - Added method to find if a record of balance exist

--- evenly_backend/src/main/java/com/evenly/repository/BalanceRepository.java
+++ evenly_backend/src/main/java/com/evenly/repository/BalanceRepository.java
@@ -4,4 +4,5 @@ import com.evenly.entity.Balance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
+    boolean existsByGroupIdAndUserIdAndOwnedTo(String groupId, String userId, String ownedTo);
 }

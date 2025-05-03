68db8372d3584f1dc80fe7825e8e4ae4df6823e0
nghiantm <nguyentrongminhnghia@gmail.com>
2025-05-02 21:48:14 -0400

    BalanceController.java - Change import

--- evenly_backend/src/main/java/com/evenly/controller/BalanceController.java
+++ evenly_backend/src/main/java/com/evenly/controller/BalanceController.java
@@ -6,10 +6,7 @@ import com.evenly.service.BalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,5 +37,4 @@ public class BalanceController {
                 HttpStatus.OK
         );
     }
-
 }
